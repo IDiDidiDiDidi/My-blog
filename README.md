d8ecf708d4d26ac56f015a6bb3894832a89eff31
liudi <https://liudi@p.mbook.cc>
2020-11-12 17:15:29 +0800

    新加登录前端页面

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # My-blog
+
+http://localhost:8080/login.html 登录页面
--- src/main/java/com/liudi/controller/AdminUserController.java
+++ src/main/java/com/liudi/controller/AdminUserController.java
@@ -8,7 +8,6 @@ import com.liudi.service.AdminUserService;
 import com.liudi.utils.PageUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
