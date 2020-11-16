ef95b13b99d28a8df93dcc9984f20f8e6c1c5892
liudi <https://liudi@p.mbook.cc>
2020-11-16 16:22:43 +0800

    新增修改管理员信息

---
+++ src/main/java/com/liudi/config/MyBlogWebMvcConfigurer.java
@@ -0,0 +1,29 @@
+package com.liudi.config;
+
+import com.liudi.interceptor.AdminLoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author liudi
+ * @version 2020/11/16 下午3:24
+ */
+@Configuration
+public class MyBlogWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    AdminLoginInterceptor adminLoginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 添加一个拦截器，拦截以/admin为前缀的url路径
+        registry.addInterceptor(adminLoginInterceptor)
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin/login")
+                .excludePathPatterns("/admin/dist/**")
+                .excludePathPatterns("/admin/plugins/**");
+    }
+}
--- src/main/java/com/liudi/service/AdminUserService.java
+++ src/main/java/com/liudi/service/AdminUserService.java
@@ -6,4 +6,9 @@ public interface AdminUserService {
 
     AdminUser login(String userName, String password);
 
+    AdminUser getUserDetailById(Integer loginUserId);
+
+    Boolean updatePassword(Integer loginUserId, String originalPassword, String newPassword);
+
+    Boolean updateName(Integer loginUserId, String loginUserName, String nickName);
 }
